cbbba1a5ec787298a9e27b587751ebd15ada24b3
Richard Nascimento <rcdwoods@gmail.com>
2022-11-18 12:20:49 -0300

    :hammer: Create Bank page

---
+++ src/app/banks/banks.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banks',
+  templateUrl: './banks.component.html',
+  styleUrls: ['./banks.component.css']
+})
+export class BanksComponent implements OnInit {
+
+  selectedOption = 'corrente';
+  banks = [
+    {
+      name: 'ITAU',
+      imgUrl: '../../assets/imgs/banks/itau.png',
+      color: '#EF761C',
+      accountType: 'Conta corrente',
+      agency: '0134',
+      accountNumber: '1242-3',
+      balance: 'R$ 13,24'
+    },
+    {
+      name: 'NUBANK',
+      imgUrl: '../../assets/imgs/banks/nubank.png',
+      color: '#781BC9',
+      accountType: 'Conta poupança',
+      agency: '1759',
+      accountNumber: '8255-7',
+      balance: 'R$ 100,00'
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getStyle(bank: any) {
+    let styles = {
+      'background-color': bank.color
+    }
+    return styles
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option
+  }
+
+}
