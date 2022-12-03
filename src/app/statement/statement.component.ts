c64f26302c66476146649b8459482216aa794907
Richard Nascimento <rcdwoods@gmail.com>
2022-12-02 21:20:16 -0300

    :hammer: Improve statement page

--- src/app/statement/statement.component.ts
+++ src/app/statement/statement.component.ts
@@ -1,9 +1,15 @@
+import { Transaction } from './../resources/models/Transaction';
+import { LoginService } from './../resources/services/login.service';
+import { BankAccountBasic } from './../resources/models/BankAccountBasic';
+import { TransferenceService } from './../resources/services/transference.service';
+import { DateTransaction } from './../resources/models/DateTransaction';
 import { BalanceDetails } from './../resources/models/BalanceDetails';
 import { CostumerService } from './../resources/services/costumer.service';
 import { BankAccount } from './../resources/models/BankAccount';
 import { Bank } from './../resources/models/Bank';
 import { BanksService } from './../resources/services/banks.service';
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-statement',
@@ -28,8 +34,15 @@ export class StatementComponent implements OnInit {
   ]
   customerBanks: BankAccount[] = []
   balanceDetails?: BalanceDetails;
+  dateTransactions?: DateTransaction[]
 
-  constructor(private banksService: BanksService, private costumerService: CostumerService) { }
+  constructor(
+    private loginService: LoginService,
+    private banksService: BanksService,
+    private costumerService: CostumerService,
+    private transferenceService: TransferenceService,
+    private datePipe: DatePipe
+    ) { }
 
   ngOnInit(): void {
     this.banksService.getBanks().subscribe(
@@ -38,6 +51,40 @@ export class StatementComponent implements OnInit {
     this.costumerService.getBalanceDetails().subscribe(
       (data) => this.balanceDetails = Object.assign(new BalanceDetails(), data)
     )
+    this.transferenceService.getTransactions().subscribe(
+      (data) => {
+        this.dateTransactions = data.map(dateTransaction => Object.assign(new DateTransaction, dateTransaction))
+        console.log(this.dateTransactions)
+      }
+    )
   }
 
+  formattedType(transaction: Transaction) {
+    if (transaction.type === 'TRANSFERENCE') return 'Transferência'
+    return 'Pix'
+  }
+
+  getValueFormatted(transaction: Transaction) {
+    let symbol = transaction.flow === 'INBOUND' ? '+' : '-'
+    let value = new String(transaction.value!!)
+    if (!value.includes(',')) value = value.concat(',00')
+    return `${symbol} R$ ${value?.replace('.', ',')}`
+  }
+
+  getFormattedHours(transaction: Transaction) {
+    console.log(transaction.performed_at)
+    return this.datePipe.transform(transaction.performed_at, 'HH:mm')
+  }
+
+  getBank(transaction: Transaction) {
+    let user = this.loginService.getUser()
+    if (transaction.type === 'INBOUND' && transaction.beneficiary_id === user.id) return transaction.beneficiary?.bank
+    return transaction.source?.bank
+  }
+
+  getAccount(transaction: Transaction) {
+    let user = this.loginService.getUser()
+    if (transaction.type === 'INBOUND' && transaction.beneficiary_id === user.id) return transaction.beneficiary?.account
+    return transaction.source?.account
+  }
 }
