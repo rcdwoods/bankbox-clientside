07e98b6eb5c089d983f44e1cf715d3ba2d08d76d
Richard Nascimento <rcdwoods@gmail.com>
2022-12-02 21:17:42 -0300

    :hammer: Add new fields to Transaction class

--- src/app/resources/models/Transaction.ts
+++ src/app/resources/models/Transaction.ts
@@ -1,8 +1,14 @@
+import { Bank } from './Bank';
+import { BankAccountBasic } from './BankAccountBasic';
 export class Transaction {
+  public source?: BankAccountBasic
   public source_id?: number
+  public beneficiary?: BankAccountBasic
   public beneficiary_id?: number
   public type?: string
   public value?: string
+  public performed_at?: string
+  public flow?: string
 
   public constructor(source_id?: number, beneficiary_id?: number, type?: string, value?: string) {
     this.source_id = source_id
@@ -11,7 +17,8 @@ export class Transaction {
     this.value = value
   }
 
-  public getValueFormatted() {
-    return this.value?.replace('.', ',')
+  getValueFormatted() {
+    let value = new String(this.value!!)
+    return value?.replace('.', ',')
   }
 }
