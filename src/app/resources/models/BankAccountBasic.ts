23726d635a818f938702af8dace6d00fa12b094d
Richard Nascimento <rcdwoods@gmail.com>
2022-12-02 21:15:18 -0300

    :hammer: Add model to represent basic informations of an account

---
+++ src/app/resources/models/BankAccountBasic.ts
@@ -0,0 +1,9 @@
+import { Bank } from './Bank';
+
+export class BankAccountBasic {
+  public id?: number
+  public customer_first_name?: string
+  public agency?: string
+  public account?: string
+  public bank?: Bank
+}
